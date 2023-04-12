44e789fb5e7ad6f59594f04984fceb38b925aa37
touchselect <kir191474@gmail.com>
2023-04-12 15:40:53 +0000

    finish

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -4,9 +4,13 @@ class ListsController < ApplicationController
   end
 
   def create
-    list = List.new(list_params)
-    list.save
-    redirect_to list_path(list.id)
+    @list = List.new(list_params)
+    if @list.save
+      flash[:notice] = "投稿が成功しました。"
+      redirect_to list_path(@list.id)
+    else
+      render :new
+    end
   end
 
   def index
